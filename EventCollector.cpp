8349c8c577e90e6a9f29f0e7e9d9feb82fab7f17
zyond26 <zyond26@gmail.com>
2025-12-03 21:53:54 +0700

    demo

---
+++ EventCollector.cpp
@@ -0,0 +1,107 @@
+﻿#include <iostream>
+#include <chrono>
+#include <thread>
+#include <string>
+#include <winsock2.h>
+#pragma comment(lib, "ws2_32.lib")
+#include <windows.h>
+#include "EventStruct.h"
+#include <ws2tcpip.h>
+
+
+// ===================== TIME ==========================
+std::string Now()
+{
+    SYSTEMTIME t;
+    GetLocalTime(&t);
+    char buf[64];
+    sprintf_s(buf, "%04d-%02d-%02d %02d:%02d:%02d",
+        t.wYear, t.wMonth, t.wDay, t.wHour, t.wMinute, t.wSecond);
+    return buf;
+}
+
+// ===================== SOCKET GLOBAL ==================
+SOCKET g_sock = INVALID_SOCKET;
+
+bool InitSocket()
+{
+    WSADATA wsa;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+        return false;
+
+    g_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (g_sock == INVALID_SOCKET) return false;
+
+    sockaddr_in server;
+    server.sin_family = AF_INET;
+    server.sin_port = htons(5050);
+    //server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    InetPton(AF_INET, L"127.0.0.1", &server.sin_addr);
+
+
+    if (connect(g_sock, (sockaddr*)&server, sizeof(server)) < 0)
+        return false;
+
+    return true;
+}
+
+void CloseSocket()
+{
+    closesocket(g_sock);
+    WSACleanup();
+}
+
+void SendEvent(const SysEvent& e)
+{
+    std::string msg = e.time + "|" + e.type + "|" + e.detail;
+    send(g_sock, msg.c_str(), msg.size(), 0);
+}
+
+// =================== PROTOTYPES ===================
+bool CheckNewProcess(SysEvent& e);
+bool MonitorDirectory(SysEvent& e);
+// bool MonitorTCPConnections(SysEvent& e);
+
+// ======================= MAIN ======================
+int main()
+{
+    if (!InitSocket())
+    {
+        std::cout << "Không thể kết nối GUI!\n";
+        return 0;
+    }
+
+    std::cout << "=== EventCollector running ===\n";
+
+    while (true)
+    {
+        // ==== PROCESS EVENT ====
+        {
+            SysEvent ev;
+            if (CheckNewProcess(ev))
+            {
+                ev.time = Now();
+                ev.type = "Process";
+                SendEvent(ev);
+                std::cout << "[Process] " << ev.detail << "\n";
+            }
+        }
+
+        // ==== FILE EVENT ====
+        {
+            SysEvent ev;
+            if (MonitorDirectory(ev))
+            {
+                ev.time = Now();
+                ev.type = "File";
+                SendEvent(ev);
+                std::cout << "[File] " << ev.detail << "\n";
+            }
+        }
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    }
+
+    CloseSocket();
+    return 0;
+}
---
+++ ProcessMonitor.cpp
@@ -0,0 +1,48 @@
+﻿#include <windows.h>
+#include <tlhelp32.h>
+#include "EventStruct.h"
+#include <set>
+#include <string>
+
+std::set<std::wstring> g_knownProcesses;
+
+// Lấy process đang chạy
+void SnapshotProcesses(std::set<std::wstring>& out)
+{
+    out.clear();
+    HANDLE snap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (snap == INVALID_HANDLE_VALUE) return;
+
+    PROCESSENTRY32 pe;
+    pe.dwSize = sizeof(pe);
+
+    if (Process32First(snap, &pe)) {
+        do {
+            out.insert(pe.szExeFile);
+        } while (Process32Next(snap, &pe));
+    }
+    CloseHandle(snap);
+}
+
+// Kiểm tra process mới
+bool CheckNewProcess(SysEvent& eventOut)
+{
+    std::set<std::wstring> current;
+    SnapshotProcesses(current);
+
+    for (auto& p : current)
+    {
+        if (!g_knownProcesses.count(p))
+        {
+            // Process mới xuất hiện
+            g_knownProcesses.insert(p);
+
+            eventOut.type = "Process";
+            eventOut.time = "TODO";               // sẽ set time ở main
+            eventOut.detail = std::string(p.begin(), p.end());
+            return true;
+        }
+    }
+
+    return false;
+}
